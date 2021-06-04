96e69885193fe5c093e7da30143b9fc5b5eb7905
wnsgud0428 <wnsgud0428@naver.com>
2021-06-04 22:33:51 +0900

    insert 안되는거 조금 수정

--- src/components/GoogleCalendarModule.vue
+++ src/components/GoogleCalendarModule.vue
@@ -71,40 +71,7 @@ export default {
         }
       }
 
-      // Check if we a busy and have an event on our calendar for the same time.
-      calendar.freebusy.query(
-        {
-          resource: {
-            timeMin: eventStartTime,
-            timeMax: eventEndTime,
-            timeZone: '',
-            items: [{ id: 'primary' }]
-          }
-        },
-        (err, res) => {
-          // Check for errors in our query and log them if they exist.
-          if (err) return console.error('Free Busy Query Error: ', err)
-
-          // Create an array of all events on our calendar during that time.
-          const eventArr = res.data.calendars.primary.busy
-
-          // Check if event array is empty which means we are not busy
-          if (eventArr.length === 0) { // If we are not busy create a new calendar event.
-            return calendar.events.insert(
-              { calendarId: 'primary', resource: event },
-              err => {
-              // Check for errors and log them if they exist.
-                if (err) return console.error('Error Creating Calender Event:', err)
-                // Else log that the event was created.
-                return console.log('Calendar event successfully created.')
-              }
-            )
-          }
-
-          // If event array is not empty log that we are busy.
-          return console.log('Sorry I\'m busy...')
-        }
-      )
+      calendar.events.insert({ calendarId: 'primary', resource: event })
     }
   }
 }
