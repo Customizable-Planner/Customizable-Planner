3c9a1c75c5dfcac5c7e2cf332c85ebb27cf8914b
wnsgud0428 <wnsgud0428@naver.com>
2021-06-06 22:15:56 +0900

    main.js에 firebase사용할 수 있도록 설정

--- src/main.js
+++ src/main.js
@@ -6,12 +6,25 @@ import vuetify from './plugins/vuetify'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 import VueDraggableResizable from 'vue-draggable-resizable'
+import firebase from 'firebase'
 
 // optionally import default styles
 import 'vue-draggable-resizable/dist/VueDraggableResizable.css'
 
 Vue.component('vue-draggable-resizable', VueDraggableResizable)
 Vue.config.productionTip = false
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBrf5VfMk8z10vCzOoXDbwI3DGKMsZIbj0',
+  authDomain: 'calendar-db111.firebaseapp.com',
+  projectId: 'calendar-db111',
+  storageBucket: 'calendar-db111.appspot.com',
+  messagingSenderId: '150863325673',
+  appId: '1:150863325673:web:1d1398876934b7f2654378',
+  measurementId: 'G-GNNF9LS7N4'
+})
+
+export const db = firebase.firestore()
 
 new Vue({
   router,
